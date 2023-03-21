9f9b0b8d62e96e594ff7baf9356f0564e1fc9ff3
KhachatryanArtur <120027657+KhachatryanArtur@users.noreply.github.com>
2023-03-21 15:41:40 +0200

    Update praktiline.py

--- praktiline/praktiline.py
+++ praktiline/praktiline.py
@@ -1,67 +1,44 @@
-from tkinter import *
-import math
+import tkinter as tk
+from tkinter.font import Font
 
-def solve():
-    a = a_entry.get()
-    b = b_entry.get()
-    c = c_entry.get()
-    
-    if not a:
-        a_entry.configure(bg="pink")
-        return
-    if not b:
-        b_entry.configure(bg="pink")
-        return
-    if not c:
-        c_entry.configure(bg="pink")
-        return
-    
-    a_entry.configure(bg="white")
-    b_entry.configure(bg="white")
-    c_entry.configure(bg="white")
-    
-    a = float(a)
-    b = float(b)
-    c = float(c)
-    
-    D = b**2 - 4*a*c
-    
-    if D < 0:
-        result_label.config(text="Juuri pole")
-    elif D == 0:
-        x = -b / (2*a)
-        result_label.config(text=f"Üks juur: x = {x}")
-    else:
-        x1 = (-b + math.sqrt(D)) / (2*a)
-        x2 = (-b - math.sqrt(D)) / (2*a)
-        result_label.config(text=f"Kaks juuri: x1 = {x1}, x2 = {x2}")
-    
 
-root = Tk()
-root.title("Ruutvõrrandi lahendamine")
+def show_bahamas_flag():
+    canvas.delete("all")
+    canvas.create_rectangle(30,50, 300,150,fill="cyan")
+    canvas.create_rectangle(30,100, 300,150,fill="yellow")
+    canvas.create_rectangle(30,200, 300,150,fill="cyan")
+    canvas.create_polygon(30,50,100,125,30,200, fill="black")
 
-a_label = Label(root, text="a = ")
-a_label.grid(row=0, column=0, padx=10, pady=10)
 
-a_entry = Entry(root)
-a_entry.grid(row=0, column=1, padx=10, pady=10)
+def show_estonia_flag():
+    canvas.delete("all")
+    canvas.create_rectangle(0, 50, 300, 150, fill="Blue")
+    canvas.create_rectangle(0, 100, 300, 150, fill="Black")
+    canvas.create_rectangle(0, 200, 300, 150, fill="White")
 
-b_label = Label(root, text="b = ")
-b_label.grid(row=1, column=0, padx=10, pady=10)
 
-b_entry = Entry(root)
-b_entry.grid(row=1, column=1, padx=10, pady=10)
+def show_russian_flag():
+    canvas.delete("all")
+    canvas.create_rectangle(0, 50, 300, 150, fill="White")
+    canvas.create_rectangle(0, 100, 300, 150, fill="Blue")
+    canvas.create_rectangle(0, 200, 300, 150, fill="Red")
 
-c_label = Label(root, text="c = ")
-c_label.grid(row=2, column=0, padx=10, pady=10)
 
-c_entry = Entry(root)
-c_entry.grid(row=2, column=1, padx=10, pady=10)
+def create_flag_button(text, command):
+    button_font = Font(family="Helvetica", size=14, weight="bold")
+    button = tk.Button(root, text=text, font=button_font, command=command)
+    button.pack(pady=10)
 
-solve_button = Button(root, text="Lahendada", command=solve)
-solve_button.grid(row=3, column=0, padx=10, pady=10)
 
-result_label = Label(root, text="")
-result_label.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
+root = tk.Tk()
+root.title("Флаги")
+root.configure(background="white")
+
+canvas = tk.Canvas(root, width=300, height=200, bg="white")
+canvas.pack()
+
+create_flag_button("Флаг Багамских островов", show_bahamas_flag)
+create_flag_button("Флаг Эстонии", show_estonia_flag)
+create_flag_button("Флаг России", show_russian_flag)
 
 root.mainloop()
