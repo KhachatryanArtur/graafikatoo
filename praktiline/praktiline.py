e7d22e4bf9499b285bf494a4c4a394762f14c39f
KhachatryanArtur <120027657+KhachatryanArtur@users.noreply.github.com>
2023-03-28 15:41:09 +0300

    Update praktiline.py

--- praktiline/praktiline.py
+++ praktiline/praktiline.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter.font import Font
-
+from tkinter import *
+from random import *
+from turtle import width
+from PIL import ImageGrab
 
 def show_bahamas_flag():
     canvas.delete("all")
@@ -15,7 +18,7 @@ def show_estonia_flag():
     canvas.create_rectangle(0, 50, 300, 150, fill="Blue")
     canvas.create_rectangle(0, 100, 300, 150, fill="Black")
     canvas.create_rectangle(0, 200, 300, 150, fill="White")
-
+                                                                      
 
 def show_russian_flag():
     canvas.delete("all")
@@ -25,20 +28,91 @@ def show_russian_flag():
 
 
 def create_flag_button(text, command):
+    canvas.delete("all")
     button_font = Font(family="Helvetica", size=14, weight="bold")
     button = tk.Button(root, text=text, font=button_font, command=command)
     button.pack(pady=10)
+
+def show_malevaljak():
+    canvas.delete("all")
+    cell_size = 50
+    for row in range(8):
+        for col in range(8):
+            x1 = col * cell_size
+            y1 = row * cell_size
+            x2 = x1 + cell_size
+            y2 = y1 + cell_size
+            if (row + col) % 2 == 0:
+                canvas.create_rectangle(x1, y1, x2, y2, fill="white")
+            else:
+                canvas.create_rectangle(x1, y1, x2, y2, fill="black")
+                
+def show_oval():
+    canvas.delete("all")
+    colors=["black",
+        "cyan",
+        "magenta",
+        "red",
+        "blue",
+        "gray",
+        "yellow",
+        "green",
+        "lightblue",
+        "pink",
+        "gold"]
+    x0 = 0
+    y0 = 0
+    x1 = 500
+    y1 = 500
+    p = 2
+    for i in range(55):
+        x0 += p
+        y0 += p
+        x1 -= p
+        y1 -= p
+        canvas.create_oval(x0, y0, x1, y1, fill=choice(colors))
+
+def show_valgusfloor():
+    canvas.delete("all")
+    canvas.create_line(60, 50, 100, 50,width=45, fill=("Red"))
+    canvas.create_line(60, 100, 100, 100, width=45, fill=("Orange"))
+    canvas.create_line(60, 150, 100, 150, width=45,fill=("Green"))
+    canvas.create_line(80, 180, 80, 300, width=8, fill=("Black"))
+    canvas.create_line(30, 310, 140, 310, width=4, fill=("Black"))
+
+def show_ruutringis():
+    canvas = Canvas(root, width=600, height=600, background="#630202")
+    k=7
+    x0=0
+    y0=0
+    x1=550
+    y1=550
+    for i in range(k):
+        x0+=50
+        y0+=50
+        x1-=50
+        y1-=50
+        canvas.create_rectangle(x0,y0,x1,y1,width=1,outline="blue", fill="Red")
+        canvas.create_oval(x0,y0,x1,y1,width=1, outline="blue", fill="Yellow")
+        canvas.grid()
+
+        ps = canvas.postscript(colormode='color')
+        x1=canvas.winfo_width()
+        y1=canvas.winfo_height()
 
 
 root = tk.Tk()
 root.title("Флаги")
 root.configure(background="white")
 
-canvas = tk.Canvas(root, width=300, height=200, bg="white")
+canvas = tk.Canvas(root, width=500, height=500, bg="white")
 canvas.pack()
 
 create_flag_button("Флаг Багамских островов", show_bahamas_flag)
 create_flag_button("Флаг Эстонии", show_estonia_flag)
 create_flag_button("Флаг России", show_russian_flag)
-
+create_flag_button("Шахматное поле", show_malevaljak)
+create_flag_button("Разноцветный овал", show_oval)
+create_flag_button("Светофор", show_valgusfloor)
+create_flag_button("Квадрат в круге", show_ruutringis)
 root.mainloop()
